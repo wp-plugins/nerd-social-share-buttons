2dcca9df3931648528d15569910fc288d03069d3
360crest <360crest@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-20 19:22:14 +0000

    adding the olugin files
    git-svn-id: https://plugins.svn.wordpress.org/nerd-social-share-buttons/trunk@1164430 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ nerd-social-plugin.php
@@ -0,0 +1,264 @@
+<?php
+/*
+Plugin Name: Nerd Social Share Buttons
+Plugin URI: http://shoutershub.com
+Description: A set of very simple social sharing buttons - featuring important social media in your network
+Author: Neon Emmanuel
+Version: 1.0
+Author URI: http://shoutershub.com
+*/
+
+// Initialize setting options on activation
+register_activation_hook( __FILE__, 'nd_install_activate_default_values' );
+function nd_install_activate_default_values() {
+$nd_plugin_options = array(
+'label' => 'Seriously !! we love Social Media :)',
+'post' => 'ok',
+'page' => '',
+);
+update_option( 'nerd_social_plugin', $nd_plugin_options );
+}
+
+
+// get option value from the database
+$option = get_option( 'nerd_social_plugin' );
+$label = $option['label'];
+	$displaypost = $option['post'];
+	$displaypage = $option['page'];
+
+// social share code obsolete
+$plugin_code = '<div id="menu-holder">
+
+<div class="set-2">
+<ul>
+<li><a href="http://twitter.com/home?status='. the_title('', '', FALSE) .': '. get_permalink() .'" title="Share this post on Twitter!" target="_blank" rel="nofollow" class="twitter-big">Twitter</a></li>
+<li><a href="http://www.facebook.com/sharer.php?u='. get_permalink() .'&amp;t='. the_title('', '', FALSE) .'" rel="nofollow" title="Share this post on Facebook!" onclick="window.open(this.href); return false;" class="facebook-big">Facebook</a></li>
+<li><a href="http://www.pinterest.com/pin/create/button/?url='. get_permalink() .' class="pinterest-big" class="pinterest-big">Pinterest</a></li>
+<li><a href="https://plus.google.com/share?url='. get_permalink() .'" rel="nofollow" class="gplus-big">GPlus</a></li>
+<li><a href="http://www.linkedin.com/shareArticle?mini=true&amp;url='. get_permalink() .'" rel="nofollow" class="linkedin-big">Linkedin</a></li>
+<li><a href="http://reddit.com/submit?url='. get_permalink() .'&amp;title='. the_title('', '', FALSE) .'" rel="nofollow" class="flickr-big">Reddit</a></li>
+</ul>
+</div>'."
+</div>
+</div>";
+
+
+add_action( 'admin_menu', 'nd_social_share_plugin' );
+
+// Adding Submenu to settings
+function nd_social_share_plugin() {
+	add_options_page( 'Don Caprio\'s Social Share Plugin', 'Don Caprio\'s Social Share',
+'manage_options', 'nerd-social-plugin-nd', 'nerd_social_plugin_nd' );
+}
+
+// plugin settings form
+function nerd_social_plugin_nd() {
+
+	?>
+<div class="wrap">
+<?php screen_icon(); ?>
+<h2>nerd's social share buttons</h2>
+<form action="options.php" method="post">
+<table class="form-table">
+<?php settings_fields('nerd_social_plugin'); ?>
+<?php do_settings_sections('nerd-social-plugin-nd'); ?>
+<div>
+<input name="Submit" class="button-primary" type="submit" value="Save Changes" />
+</div>
+
+</form>
+</table>
+
+<br />
+<h2>Documentation</h2>
+<h3>Template Tags</h3>
+<table><tr valign='top'>
+<td scope='row'><p style="list-style:circle"></p>To include this plugin in any location of your theme, <br />
+use this template tag</p></td>
+<td style="margin: 5px"><textarea cols='30' rows='1'>&lt;?php nd_social_share_tag(); ?&gt;</textarea></td>
+</tr>
+
+<tr valign='top'>
+<td scope='row'><p style="list-style:circle"></p>To add a <b>Share label</b> using the template tag,<br />
+pass the text as the function argument like this: </p></td>
+<td><textarea cols='45' rows='1'>&lt;?php nd_social_share_tag('Share this article'); ?&gt;</textarea></td>
+</tr></table>
+
+<h3>Shortcodes</h3>
+<table><tr valign='top'>
+<td scope='row'><p style="list-style:circle"></p>To include this plugin within a post or page,<br />
+use this Shortcodes</p></td>
+<td style="margin: 5px"><textarea cols='30' rows='1'>[nd-social]</textarea></td>
+</tr>
+
+<tr valign='top'>
+<td scope='row'><p style="list-style:circle"></p>To add a <b>share label</b> using shortcodes,<br />
+do it like this: </p></td>
+<td><textarea cols='45' rows='1'>[nd-social label="Share this article"]</textarea></td>
+</tr></table>
+</div>
+
+<?php }
+
+	// Register and define the settings
+	add_action('admin_init', 'nerd_social_plugin_init');
+	function nerd_social_plugin_init(){
+	register_setting(
+	'nerd_social_plugin',
+	'nerd_social_plugin',
+	''
+	);
+
+	add_settings_section(
+	'nerd-social-plugin-main',
+	'',
+	'',
+	'nerd-social-plugin-nd'
+	);
+	add_settings_field(
+	'nerd-social-plugin-nd',
+	'Text to display before social buttons 	',
+	'nd_social_share_plugin_setting_input',
+	'nerd-social-plugin-nd',
+	'nerd-social-plugin-main'
+	);
+	}
+
+	// Display and fill the form field
+	function nd_social_share_plugin_setting_input() {
+	// Retrieve the settings values form DB and make them global
+	global $label, $displaypost, $displaypage;
+	echo "
+	<tr valign='top'>
+	<th scope='row'><label for='read_more'> <strong>Sharing label</strong></label></th>";
+	echo "<td><textarea cols='60' rows='2' id='label' name='nerd_social_plugin[label]'>$label</textarea></td>
+	</tr>"; ?>
+	<tr valign='top'>
+	<th scope='row'><label for='display'> <strong>Where to be shown?</strong></label></th>
+	<td><b>Post</b><br/><input type='checkbox' name='nerd_social_plugin[post]' value="ok" <?php checked($displaypost, 'ok')?> /><br/>
+	<br/><b>Page</b><br/><input type='checkbox' name='nerd_social_plugin[page]' value="ok" <?php checked($displaypage, 'ok')?> /><br/>
+	</td><br/><br/>
+	</tr>
+	<?php
+	}
+
+	/**
+	* Enqueue plugin font and css
+	*/
+	function nd_social_share_css() {
+	wp_enqueue_style( 'nd-social-share', plugins_url('nd-social-share.css', __FILE__) );
+	wp_enqueue_style( 'nd-social-font', 'http://fonts.googleapis.com/css?family=Pacifico' );
+	}
+	add_action( 'wp_enqueue_scripts', 'nd_social_share_css' );
+
+	add_action('the_content', 'nd_social_share_plugin_display');
+
+	/**
+	* Displays social button function
+	* @param string $content post content
+	*/
+	function nd_social_share_plugin_display($content) {
+	global $plugin_code, $label, $displaypost, $displaypage;
+	
+	if ($displaypost == 'ok') {
+	if (is_single()) {
+	$content.= '<div class="myclass" align="center">
+	<div class="social"><div class="thetext">'. $label . '<div id="menu-holder">
+
+<div class="set-2">
+<ul>
+<li><a href="http://twitter.com/home?status='. the_title('', '', FALSE) .': '. get_permalink() .'" title="Share this post on Twitter!" target="_blank" rel="nofollow" class="twitter-big">Twitter</a></li>
+<li><a href="http://www.facebook.com/sharer.php?u='. get_permalink() .'&amp;t='. the_title('', '', FALSE) .'" rel="nofollow" title="Share this post on Facebook!" onclick="window.open(this.href); return false;" class="facebook-big">Facebook</a></li>
+<li><a href="http://www.pinterest.com/pin/create/button/?url='. get_permalink() .' class="pinterest-big" class="pinterest-big">Pinterest</a></li>
+<li><a href="https://plus.google.com/share?url='. get_permalink() .'" rel="nofollow" class="gplus-big">GPlus</a></li>
+<li><a href="http://www.linkedin.com/shareArticle?mini=true&amp;url='. get_permalink() .'" rel="nofollow" class="linkedin-big">Linkedin</a></li>
+<li><a href="http://reddit.com/submit?url='. get_permalink() .'&amp;title='. the_title('', '', FALSE) .'" rel="nofollow" class="flickr-big">Reddit</a></li>
+</ul>
+</div>'."
+</div>
+</div>
+</div>";
+	}	
+	}
+if ($displaypage == 'ok') {
+	if (is_page()) {
+	$content.= '<div class="myclass" align="center">
+	<div class="social"><div class="thetext">'. $label  . '</div>
+       <div id="menu-holder">
+
+<div class="set-2">
+<ul>
+<li><a href="http://twitter.com/home?status='. the_title('', '', FALSE) .': '. get_permalink() .'" title="Share this post on Twitter!" target="_blank" rel="nofollow" class="twitter-big">Twitter</a></li>
+<li><a href="http://www.facebook.com/sharer.php?u='. get_permalink() .'&amp;t='. the_title('', '', FALSE) .'" rel="nofollow" title="Share this post on Facebook!" onclick="window.open(this.href); return false;" class="facebook-big">Facebook</a></li>
+<li><a href="http://www.pinterest.com/pin/create/button/?url='. get_permalink() .' class="pinterest-big" class="pinterest-big">Pinterest</a></li>
+<li><a href="https://plus.google.com/share?url='. get_permalink() .'" rel="nofollow" class="gplus-big">GPlus</a></li>
+<li><a href="http://www.linkedin.com/shareArticle?mini=true&amp;url='. get_permalink() .'" rel="nofollow" class="linkedin-big">Linkedin</a></li>
+<li><a href="http://reddit.com/submit?url='. get_permalink() .'&amp;title='. the_title('', '', FALSE) .'" rel="nofollow" class="flickr-big">Reddit</a></li>
+</ul>
+</div>
+</div>'."
+</div>";
+	}	
+	}
+
+	return $content;
+	}
+
+	/**
+	* Shortcode function ish
+	*/
+	add_shortcode( 'nd-social', 'nd_social_share_plugin_shortcode' );
+	function nd_social_share_plugin_shortcode( $attr ) {
+	global $plugin_code;
+	echo '<div class="myclass" align="center">';
+	if (!empty($attr['label'])) {
+	echo '<div class="social"><div class="thetext">'. $attr['label'] . '</div>';
+	}
+	else {
+	echo '<div class="social">';
+	}
+	echo '<!--Facebook-->
+ <div id="menu-holder">
+
+<div class="set-2">
+<ul>
+<li><a href="http://twitter.com/home?status='. the_title('', '', FALSE) .': '. get_permalink() .'" title="Share this post on Twitter!" target="_blank" rel="nofollow" class="twitter-big">Twitter</a></li>
+<li><a href="http://www.facebook.com/sharer.php?u='. get_permalink() .'&amp;t='. the_title('', '', FALSE) .'" rel="nofollow" title="Share this post on Facebook!" onclick="window.open(this.href); return false;" class="facebook-big">Facebook</a></li>
+<li><a href="http://www.pinterest.com/pin/create/button/?url='. get_permalink() .' class="pinterest-big" class="pinterest-big">Pinterest</a></li>
+<li><a href="https://plus.google.com/share?url='. get_permalink() .'" rel="nofollow" class="gplus-big">GPlus</a></li>
+<li><a href="http://www.linkedin.com/shareArticle?mini=true&amp;url='. get_permalink() .'" rel="nofollow" class="linkedin-big">Linkedin</a></li>
+<li><a href="http://reddit.com/submit?url='. get_permalink() .'&amp;title='. the_title('', '', FALSE) .'" rel="nofollow" class="flickr-big">Reddit</a></li>
+</ul>
+</div>
+</div>
+' . " 
+</div>
+</div>";
+	}
+
+	/**
+	* Template tag for adding plugin to templates
+	* @param string $read_more Share plugin label
+	*/
+	function nd_social_share_tag( $read_more = '') {
+	global $plugin_code;
+	echo '<div class="myclass" align="center">
+	<div class="social"><div class="thetext">'. $read_more . '</div>
+	  <div id="menu-holder">
+
+<div class="set-2">
+<ul>
+<li><a href="http://twitter.com/home?status='. the_title('', '', FALSE) .': '. get_permalink() .'" title="Share this post on Twitter!" target="_blank" rel="nofollow" class="twitter-big">Twitter</a></li>
+<li><a href="http://www.facebook.com/sharer.php?u='. get_permalink() .'&amp;t='. the_title('', '', FALSE) .'" rel="nofollow" title="Share this post on Facebook!" onclick="window.open(this.href); return false;" class="facebook-big">Facebook</a></li>
+<li><a href="http://www.pinterest.com/pin/create/button/?url='. get_permalink() .' class="pinterest-big" class="pinterest-big">Pinterest</a></li>
+<li><a href="https://plus.google.com/share?url='. get_permalink() .'" rel="nofollow" class="gplus-big">GPlus</a></li>
+<li><a href="http://www.linkedin.com/shareArticle?mini=true&amp;url='. get_permalink() .'" rel="nofollow" class="linkedin-big">Linkedin</a></li>
+<li><a href="http://reddit.com/submit?url='. get_permalink() .'&amp;title='. the_title('', '', FALSE) .'" rel="nofollow" class="flickr-big">Reddit</a></li>
+</ul>
+</div>	' .
+		"
+</div>
+</div>
+</div>";
+
+	}
---
+++ readme.txt
@@ -0,0 +1,62 @@
+=== nerd Social Share Buttons ===
+Contributors: Neon Emmanuel
+Tags: facebook, twitter, google+, social, share
+Requires at least: 3.0.1
+Tested up to: 3.8.2
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+A set of very simple social sharing buttons- important in your social media network.
+
+== Description ==
+Simple and fast to load social share buttons , fancy and bold enough to get a share from your visitors, it's written in PHP, HTML and CSS. 
+
+There's an option to show the buttons on posts and pages. You can also put the buttons anywhere in your template using this template tag:
+
+`<?php nd_social_share_tag(); ?>`
+
+You can also display using this shortcode in single posts and pages:
+
+`[nd-social]`
+
+Please note that these buttons do not show share counts. The essence of the whole thing is to keep it very simplified.
+
+The only button that requires a little snippet of JS to work is the Pinterest button but this does not slow down your site in anyway. A demo of the plugin can be seen here: http://geek.ng/
+
+== Installation ==
+1. Upload 'plugin-name.php' to the '/wp-content/plugins/' directory
+2. Activate the plugin through the "Plugins" menu in WordPress
+
+You can also upload directly from Wordpress dashboard > Plugins > Add New > Upload. Activate the plugin and you're good to go.
+
+== Frequently Asked Questions ==
+
+= How do I show share counts? =
+
+This plugin isn't meant to do that. The essence of the whole thing is to keep it simplified? 
+
+= How do I show the buttons in certain areas of my websites not available through the plugin settings... for example, above posts? =
+
+You can use the template tag below:
+
+`<?php nd_social_share_tag(); ?>`
+
+You can also use this shortcode:
+
+`[nd-social]`
+
+= Can I change the "Like this article?" text? =
+
+Yes, you can use any custom text. You can set that through the settings page. If you're also using the template tag, you can simply write it this way:
+
+`<?php nd_social_share_tag('Custom Text Here'); ?>`
+
+== Screenshots ==
+1. Share buttons appearance at the frontend 
+2. Plugin backend settings
+
+== Changelog ==
+Version 1.0
+
+Initial release.
